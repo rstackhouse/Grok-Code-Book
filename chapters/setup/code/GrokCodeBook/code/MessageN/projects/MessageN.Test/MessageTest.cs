f3bd1cdc41c8e0357f3b1d55092b1b3f626eb0e0
Robert Stackhouse <robertstackhouse@gmail.com>
2011-08-30 17:02:53 -0500

    Finishing up A First Test

---
+++ chapters/setup/code/GrokCodeBook/code/MessageN/projects/MessageN.Test/MessageTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace MessagN.Test
+{
+    [TestFixture]
+    public class MessageTest
+    {
+        [Test]
+        public void Should_Set_CreationTime()
+        {
+            Assert.Fail();
+        }
+    }
+}
