602ae180132b376417dbde100ddc2b8cf3fa78c5
Robert Stackhouse <robertstackhouse@gmail.com>
2011-09-01 13:08:29 -0500

    First failing test for config chapter.

---
+++ chapters/config/document_assets/code/MessageN/projects/MessageN.Test/MessageTransportConfigurationSectionTest-01.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace MessagN.Test
+{
+    [TestFixture]
+    public class MessageTransportConfigurationSectionTest
+    {
+        [Test]
+        public void Should_Return_List_Of_Transports_From_Transports()
+        {
+            Assert.Fail();
+        }
+    }
+}
