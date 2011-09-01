3db131a6772d964c13cc04e2a05cca00f9ad8bc9
Robert Stackhouse <robertstackhouse@gmail.com>
2011-09-01 13:07:38 -0500

    Got to a stopping place on the chapter on the core components.

---
+++ chapters/classy/code/GrokCodeBook/code/MessageN/projects/MessageN/Interfaces.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+
+namespace MessageN
+{
+    public interface IUser
+    {
+        string Name {get;}
+    }
+
+    public interface IMessage
+    {
+        string Text {get;}
+        DateTime CreationTime {get;}
+        IUser From {get;}
+    }
+    
+    public interface IDeliveryAttempt
+    {
+        IMessage Message {get;}
+        DateTime Time {get;}
+        bool Success {get;}
+        string Error {get;}
+    }
+    
+    ///<summary>
+    ///Responsible for getting the message to its destination.
+    ///</summary>
+    public interface IMessageRouter
+    {
+        IMessage Message {get;}
+        List<IAttempt> Attempts {get;}
+        bool Active {get;}
+        void Send(IMessage message);
+    }
+    
+    ///<summary>
+    ///The communication channel for the message to be sent along.
+    ///</summary>
+    public interface IMessageTransport
+    {
+        bool InService { get; }
+        void Send(IMessage message);
+    }
+}
